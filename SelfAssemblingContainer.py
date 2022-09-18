c2884f32ba166580360217d9e439cc4fe0cc3bf2
lancefeig <87787937+lancefeig@users.noreply.github.com>
2022-09-17 20:14:05 -0500

    Update SelfAssemblingContainer.py

--- SelfAssemblingContainer.py
+++ SelfAssemblingContainer.py
@@ -4,10 +4,11 @@ from datetime import datetime
 import tkinter as tk
 import tkinter.ttk as ttk
 
-#Stores the directory of this .py file
-PROJECT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-
 class Interface(tk.Tk):
+    
+    #Stores the directory of this .py file
+    PROJECT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+    
     def __init__(self):
         super().__init__()
 
